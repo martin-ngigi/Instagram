87a6e83b85fcfc8403cee9731b929c46328e1da4
martin-ngigi <martinwainaina001@gmail.com>
2023-12-11 13:20:25 +0300

    Initial commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Martin Wainaina on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
